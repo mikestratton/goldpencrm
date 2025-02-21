96413e211018aab6548efa4d4d134545d43fd01c
mikestratton <mike@mikestratton.net>
2025-02-21 00:02:30 -0500

    response tables in prospects, notes and pitches

--- resources/views/salesai/edit.blade.php
+++ resources/views/salesai/edit.blade.php
@@ -4,7 +4,7 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-[#101a23] overflow-hidden shadow-sm sm:rounded-lg border border-gray-200 dark:border-[#314f68]">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <h2 class="text-2xl font-bold mb-4">Edit Pitch</h2>
+                        <h2 class="text-2xl font-bold mb-4">View/Edit Pitch</h2>
 
                         <form method="POST" action="{{ route('salesai.update', $pitch) }}">
                             @csrf
